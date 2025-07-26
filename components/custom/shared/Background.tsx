e51e0c7783308e11dac0420aa6b6a6d75c6d0306
Ronald Vergel Dela Cruz <delacruz.ronaldvergel@gmail.com>
2025-07-26 09:21:42 +0800

    chore: update bg-color

--- components/custom/shared/Background.tsx
+++ components/custom/shared/Background.tsx
@@ -13,14 +13,16 @@ export function Background() {
   const style =
     resolvedTheme === 'dark'
       ? {
-          backgroundImage: `radial-gradient(circle 600px at 50% 50%, rgba(59,130,246,0.3), transparent)`,
+          backgroundImage: `radial-gradient(circle at 50% 100%, rgba(70, 85, 110, 0.5) 0%, transparent 60%),
+          radial-gradient(circle at 50% 100%, rgba(99, 102, 241, 0.4) 0%, transparent 70%),
+          radial-gradient(circle at 50% 100%, rgba(181, 184, 208, 0.3) 0%, transparent 80%)`,
           backgroundColor: '#000000',
         }
       : {
-          background: '#ffffff',
-          backgroundImage: `radial-gradient(circle at top right, rgba(70, 130, 180, 0.5), transparent 70%)`,
-          filter: 'blur(80px)',
-          backgroundRepeat: 'no-repeat',
+          backgroundImage: `repeating-linear-gradient(22.5deg, transparent, transparent 2px, rgba(75, 85, 99, 0.06) 2px, rgba(75, 85, 99, 0.06) 3px, transparent 3px, transparent 8px),
+        repeating-linear-gradient(67.5deg, transparent, transparent 2px, rgba(107, 114, 128, 0.05) 2px, rgba(107, 114, 128, 0.05) 3px, transparent 3px, transparent 8px),
+        repeating-linear-gradient(112.5deg, transparent, transparent 2px, rgba(55, 65, 81, 0.04) 2px, rgba(55, 65, 81, 0.04) 3px, transparent 3px, transparent 8px),
+        repeating-linear-gradient(157.5deg, transparent, transparent 2px, rgba(31, 41, 55, 0.03) 2px, rgba(31, 41, 55, 0.03) 3px, transparent 3px, transparent 8px)`,
         }
 
   return (
