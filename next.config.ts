5773c3d2dff38478c0d731829c96a6967aa80c45
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2026-02-03 17:09:46 +0800

    fix: nextjs image config

--- next.config.ts
+++ next.config.ts
@@ -1,49 +1,49 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*/`,
-			},
-		];
-	},
-	turbopack: {
-		rules: {
-			"*.svg": {
-				loaders: ["@svgr/webpack"],
-				as: "*.js",
-			},
-		},
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "127.0.0.1",
-				port: "8000",
-				pathname: "/media/**",
-			},
-			{
-				protocol: "http",
-				hostname: "188.166.230.203",
-				port: "8000",
-				pathname: "/media/**",
-			},
-			{
-				protocol: "http",
-				hostname: "152.42.229.230",
-				port: "8000",
-				pathname: "/media/**",
-			},
-			{
-				protocol: "https",
-				hostname: "api-rvdcrefandaircon.duckdns.org",
-				pathname: "/media/**",
-			},
-		],
-	},
-};
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*/`,
+      },
+    ]
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "188.166.230.203",
+        port: "8000",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "http",
+        hostname: "152.42.229.230",
+        port: "8000",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api-rvdcrefandaircon.duckdns.org",
+        pathname: "/media/**",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
